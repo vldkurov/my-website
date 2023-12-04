b0cef65ae867858a011b2e356dcd025171059232
Volodymyr Kurov <151948182+vldkurov@users.noreply.github.com>
2023-12-04 07:47:05 +0000

    script timer update with no formatter

--- timer.js
+++ timer.js
@@ -1,13 +1,19 @@
 const timeContainer = document.getElementById("time-container");
-const START_DAY = "02-24-2022";
+// const START_DAY = "02-24-2022";
+const START_DAY = "2022-02-24";
 const START_DAY_DATE = new Date(START_DAY);
+// console.log(START_DAY_DATE);
 const intlNumberFormatter = new Intl.NumberFormat("en-UK");
 
 setInterval(() => {
     const now = new Date();
+    // console.log(now);
     const difference = Math.floor(
         (now.getTime() - START_DAY_DATE.getTime()) / 1000
     );
+    // console.log(difference);
 
-    timeContainer.innerHTML = intlNumberFormatter.format(difference);
+    // console.log(typeof intlNumberFormatter.format(difference));
+    // timeContainer.innerHTML = intlNumberFormatter.format(difference);
+    timeContainer.innerHTML = String(difference);
 }, 1000);
